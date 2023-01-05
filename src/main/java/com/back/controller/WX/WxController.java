b4d18bbfceaaa146bf3d64fd166bce5047e51eb4
杨生 <2785021810@qq.com>
2023-01-05 23:11:27 +0800

    优化代码

--- src/main/java/com/back/controller/WX/WxController.java
+++ src/main/java/com/back/controller/WX/WxController.java
@@ -1,13 +1,19 @@
 package com.back.controller.WX;
 
 import com.back.common.utils.Wxutils.EchostrCheckUtil;
+import com.back.service.MessageService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@RestController("/back")
+@RestController
 public class WxController {
+    @Resource
+    private MessageService messageService;
 
     /**
      * 微信验证接口
@@ -18,6 +24,18 @@ public class WxController {
     public String verifyToken(HttpServletRequest request){
         return EchostrCheckUtil.checkSignature(request);
     }
+
+    /**
+     * 回复微信消息
+     * @param request
+     * @param response
+     * @return
+     */
+    @PostMapping("/wx")
+    public String answerMessage(HttpServletRequest request, HttpServletResponse response){
+        return messageService.answerMessage(request,response);
+    }
+
 
     @GetMapping("/test")
     public String test(HttpServletRequest request){
