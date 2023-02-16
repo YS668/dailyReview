9ec3f12eda39b80c409479044c6b88f28ebab4fd
DESKTOP-3VJ4QNV\admin <2785021810@qq.com>
2023-02-16 14:33:26 +0800

    分组

--- src/main/java/com/back/controller/StockGroupController.java
+++ src/main/java/com/back/controller/StockGroupController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,8 +37,8 @@ public class StockGroupController {
     private StockgroupService stockgroupService;
 
     //获取所有分组
-    @GetMapping("/all")
-    public Result getAll(@RequestParam Long uid){
+    @GetMapping("/all/{uid}")
+    public Result getAll(@PathVariable Long uid){
         List<String> res = stockgroupService.getAllGroup(uid);
         return Result.suc(res);
     }
